bec079fe2b7338a6fc79ccd97e9befc223635cc2
Thomas Temple <tjt_84@hotmail.com>
2021-05-29 20:05:40 +1000

    some variables

---
+++ some-ideas.cs
@@ -0,0 +1,13 @@
+using System;
+class Hello
+{
+  static void Main(string[] args)
+  {
+    string hello = "Hello World!!!";
+    double newWord = 00.12;
+    int integer = 12;
+    char test = 'a';
+    bool trueOrFalse = true;
+      Console.WriteLine(hello);
+  }
+}
