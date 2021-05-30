c7e677d3a5452767c57b3172d1fb35db5f8db593
Thomas Temple <tjt_84@hotmail.com>
2021-05-30 13:00:44 +1000

    on to methods

--- some-ideas.cs
+++ some-ideas.cs
@@ -65,10 +65,27 @@ class Hello
 // TERNERARY OPERATOR
 
         int pepperLength = 4;
-
         string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
-
         Console.WriteLine(message);
 
+  }
+
+
+// METHODS WITH MULTIPLE PERAMETERS
+    static void Main(string[] args)
+    {
+      VisitPlanets(numberOfPlanets : 2, name : "Thomas");
+    }
+
+    static void VisitPlanets(
+      string adjective = "brave",
+      string name = "Cosmonaut",
+      int numberOfPlanets = 0,
+      double gForce = 4.2)
+    {
+      Console.WriteLine($"Welcome back, {adjective} {name}.");
+      Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+      Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+    }
   }
 }
