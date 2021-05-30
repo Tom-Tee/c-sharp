bd9e99120a2eaf649d5173c786b4a584cb5b5e18
Thomas Temple <tjt_84@hotmail.com>
2021-05-30 12:24:38 +1000

    ternerary op

--- some-ideas.cs
+++ some-ideas.cs
@@ -62,5 +62,13 @@ class Hello
       }
 
 
+// TERNERARY OPERATOR
+
+        int pepperLength = 4;
+
+        string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+
+        Console.WriteLine(message);
+
   }
 }
